de5685cb4731a9be58690607ee91fd2e87173edf
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-20 16:51:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (dwarves)
   # Your code here
+  dwarves.each_with_index do |x,index|
+    puts "#{index+1} #{x}"
+  end 
+    
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (planeteer_calls)
   # Your code here
+  planeteer_calls.collect do |x|
+    x[0] = x[0].upcase
+    x << "!"
+  end 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (calls)
   # Your code here
+  calls.any? {|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.find do |x| 
+    cheese_types.find do |y|
+      x == y
+    end 
+  end 
 end
+
